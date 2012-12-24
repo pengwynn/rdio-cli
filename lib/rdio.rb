325608aa7d7ded9705e56eeb12a742bd7a7d7add
Wynn Netherland <wynn@github.com>
2012-12-23 22:17:31 -0600

    Move commands to module

--- lib/rdio.rb
+++ lib/rdio.rb
@@ -1,7 +1,243 @@
-require 'rdio/version'
-require 'api'
+require 'gli'
 require 'highline/import'
 require 'yaml'
 
-# Add requires for other files you add to your project here, so
-# you just need to require this one file in your bin file
+require 'api'
+require 'rdio/version'
+
+module Rdio
+  extend GLI::App
+
+  program_desc 'Simple CLI for Rdio'
+
+  version Rdio::VERSION
+
+  def self.api
+    token = @access_token ? [@access_token, @access_secret] : nil
+    @api = Api.new \
+      [@consumer_key, @consumer_secret],
+      token
+  end
+
+  def self.authorize_api
+    url = api.begin_authentication('oob')
+    puts url
+    ask 'Copy the four digit code from your browser. ENTER to continue.'
+    `open #{url}`
+    code = ask 'Code:'
+    @access_token, @access_secret = @api.complete_authentication(code)
+
+    write_config
+
+    puts "You're all set. see `rdio help` for usage"
+  end
+
+  def self.rdio_config
+    {
+     :consumer_key    => @consumer_key,
+     :consumer_secret => @consumer_secret,
+     :access_token    => @access_token,
+     :access_secret   => @access_secret
+    }
+  end
+
+  def self.write_config
+    File.open( @config_file, 'w' ) do |out|
+      YAML.dump rdio_config, out
+    end
+  end
+
+  def self.apple_script(cmd)
+    `osascript -e '#{cmd}'`
+  end
+
+  def self.tell_rdio(cmd)
+    apple_script "tell app \"Rdio\" to #{cmd}"
+  end
+
+  def self.current_track
+    tell_rdio('name of the current track').gsub(/\n/, '')
+  end
+
+  def self.current_artist
+    tell_rdio('artist of the current track').gsub(/\n/, '')
+  end
+
+  def self.current_album
+    tell_rdio('album of the current track').gsub(/\n/, '')
+  end
+
+  def self.display_track
+    puts "Now Playing: #{current_track} / #{current_artist} / #{current_album}"
+  end
+
+  def self.set_volume(pct = 30)
+    tell_rdio "set the sound volume to #{pct}"
+  end
+
+  def self.rdio_url
+    path = tell_rdio 'rdio url of the current track'
+    "http://www.rdio.com#{path}"
+  end
+
+  config_file '.rdio'
+
+  flag :consumer_key
+  flag :consumer_secret
+  flag :access_token
+  flag :access_secret
+
+  desc 'Plays the current track'
+  command :play do |c|
+    c.action do |global_options,options,args|
+      tell_rdio "play"
+    end
+  end
+
+  desc 'Pause the player'
+  skips_pre
+  command :pause do |c|
+    c.action do |global_options,options,args|
+      tell_rdio "pause"
+    end
+  end
+
+  desc 'Toggle playback'
+  skips_pre
+  command :toggle do |c|
+    c.action do |global_options,options,args|
+      tell_rdio "playpause"
+    end
+  end
+
+  desc 'Display the current track info'
+  skips_pre
+  command :current do |c|
+    c.action do |global_options,options,args|
+      display_track
+    end
+  end
+
+  desc 'Skip to next track'
+  skips_pre
+  command :next do |c|
+    c.action do |global_options,options,args|
+      tell_rdio "next track"
+    end
+  end
+
+  desc 'Play previous track'
+  skips_pre
+  command :previous, :prev do |c|
+    c.action do |global_options,options,args|
+      tell_rdio "previous track"
+    end
+  end
+
+  desc 'Open the current track in Rdio player'
+  skips_pre
+  command :browse do |c|
+    c.action do |global_options,options,args|
+      exec "open '#{rdio_url}'"
+    end
+  end
+
+  desc 'Set volume for player'
+  skips_pre
+  arg_name 'level'
+  command :volume, :vol do |c|
+    c.action do |global_options,options,args|
+      level = args.shift.to_i
+      set_volume level
+    end
+  end
+
+  desc 'Mute the Rdio player'
+  skips_pre
+  command :mute do |c|
+    c.action do |global_options,options,args|
+      set_volume 0
+    end
+  end
+
+  desc 'Get a shareable link for the current track'
+  skips_pre
+  command :link do |c|
+    c.action do |global_options,options,args|
+      puts rdio_url
+    end
+  end
+
+  desc "Get CLI and application version info"
+  skips_pre
+  command :version, :v do |c|
+    c.action do |global_options,options,args|
+      puts "rdio-cli #{Rdio::VERSION} / Rdio #{apple_script('get version of application "Rdio"')}"
+    end
+  end
+
+  desc "Quit Rdio"
+  skips_pre
+  command :quit, :q do |c|
+    c.action do |global_options,options,args|
+      apple_script "tell application \"Rdio\" to quit"
+    end
+  end
+
+  desc "Authorize Rdio account"
+  skips_pre
+  command :authorize, :auth do |c|
+    c.action do |global_options,options,args|
+      @consumer_key = ask 'Enter Rdio consumer key'
+      @consumer_secret = ask 'Enter Rdio consumer secret'
+
+      if @consumer_key && @consumer_secret
+        authorize_api
+      end
+    end
+  end
+
+  ### Authenticated methods
+
+  desc 'Show the current Rdio user'
+  command :user do |c|
+    c.action do |global_options,options,args|
+      user = api.call('currentUser')['result']
+      puts "#{user['firstName']} #{user['lastName']}"
+    end
+  end
+
+  pre do |global,command,options,args|
+    # Pre logic here
+    # Return true to proceed; false to abourt and not call the
+    # chosen command
+    # Use skips_pre before a command to skip this block
+    # on that command only
+    @consumer_key    = global[:consumer_key]
+    @consumer_secret = global[:consumer_secret]
+    @access_token    = global[:access_token]
+    @access_secret   = global[:access_secret]
+    if api.token.compact.empty?
+      puts 'Rdio credentials not found. Please run: rdio authorize'
+
+      false
+    else
+      true
+    end
+  end
+
+  post do |global,command,options,args|
+    # Post logic here
+    # Use skips_post before a command to skip this
+    # block on that command only
+  end
+
+  on_error do |exception|
+    # Error logic here
+    # return false to skip default error handling
+    true
+  end
+
+end
+
+
--- rdio-cli.gemspec
+++ rdio-cli.gemspec
@@ -8,17 +8,16 @@ spec = Gem::Specification.new do |s|
   s.homepage = 'http://wynnnetherland.com'
   s.platform = Gem::Platform::RUBY
   s.summary = 'CLI for Rdio for Mac'
-  s.files = %w(
-bin/rdio
-lib/rdio/version.rb
-lib/rdio.rb
-  )
+  s.files = %w(LICENSE.md README.md Rakefile rdio-cli.gemspec)
+  s.files += Dir.glob("lib/**/*.rb")
+  s.files += Dir.glob("bin/**/*")
   s.require_paths << 'lib'
   s.has_rdoc = true
   s.extra_rdoc_files = ['README.rdoc','rdio.rdoc']
   s.rdoc_options << '--title' << 'rdio' << '--main' << 'README.rdoc' << '-ri'
   s.bindir = 'bin'
   s.executables << 'rdio'
+  s.add_dependency('highline', '~> 1.6.15')
   s.add_dependency('highline', '~> 1.6.15')
   s.add_development_dependency('rake')
   s.add_development_dependency('rdoc')
